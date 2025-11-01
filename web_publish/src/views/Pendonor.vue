63937658458ace22e11ebd9cfb3d8451bc4b23d5
chelseafc2022 <rswnmrzl@gmail.com>
2025-11-01 09:21:49 +0800

    paginasi

--- web_publish/src/views/Pendonor.vue
+++ web_publish/src/views/Pendonor.vue
@@ -45,32 +45,30 @@
                             <h4><a href="javascript:void(0);">{{ donor.nama_lengkap }}</a></h4>
                             <ul>
                                 <!-- <li>081234567890</li> -->
-                                <li class="active">Golongan Darah: {{ donor.golongan_darah }}</li>
+                                <li class="active">Golongan Darah: {{ donor.golongan_darah }}{{ donor.rhesus }}</li>
                             </ul>
                             <!-- <p>MedDoctors Are A Medical And Health Department Provider Institutions. Suitable For Healthcare, Medical, Doctor, Dental, Dentist, Pharmacy, Health And Any Related Medical Care Field.......</p> -->
                             <!-- <i class="fas fa-phone"></i> -->
-                            <a :href="'https://wa.me/' + donor.no_hp" class="link fab fa-whatsapp"> Hubungi: {{ donor.no_hp }}</a>
+                            <a :href="'https://wa.me/' + donor.no_hp" class="link fab fa-whatsapp"> Hubungi: {{
+                                donor.no_hp }}</a>
                         </div>
                     </div>
 
-                    <!-- Pagination -->
-                <div class="row mt-4">
-                    <div class="col-12">
-                        <nav aria-label="Pagination">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item" :class="{ disabled: currentPage === 1 }">
-                                    <a class="page-link" href="javascript:void(0);" @click="changePage(currentPage - 1)">Previous</a>
-                                </li>
-                                <li v-for="page in totalPages" :key="page" class="page-item" :class="{ active: page === currentPage }">
-                                    <a class="page-link" href="javascript:void(0);" @click="changePage(page)">{{ page }}</a>
-                                </li>
-                                <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-                                    <a class="page-link" href="javascript:void(0);" @click="changePage(currentPage + 1)">Next</a>
-                                </li>
-                            </ul>
-                        </nav>
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="pagination-area">
+                                <ul class="pagination">
+                                    <li :class="{ disabled: currentPage === 1 }" @click="changePage(currentPage - 1)">
+                                        Prev</li>
+                                    <li v-for="page in totalPages" :key="page" :class="{ active: page === currentPage }"
+                                        @click="changePage(page)">{{ page }}</li>
+                                    <li :class="{ disabled: currentPage === totalPages }"
+                                        @click="changePage(currentPage + 1)">Next</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
                 </div>
             </div>
@@ -98,7 +96,7 @@ export default {
         this.fetchData();
     },
     methods: {
-        fetchData: function() {
+        fetchData: function () {
             fetch(this.$store.state.URL.DONOR_PUBLISH + "getview", {
                 method: "POST",
                 headers: {
@@ -109,18 +107,18 @@ export default {
                     golongan_darah: this.selectedGolongan || null
                 })
             })
-            .then(res => res.json())
-            .then(res_data => {
-                if (res_data.success) {
-                    this.donors = res_data.data;
-                    this.totalPages = res_data.pagination.totalPages;
-                    this.totalItems = res_data.pagination.totalItems;
-                    this.itemsPerPage = res_data.pagination.itemsPerPage;
-                }
-            })
-            .catch(err => {
-                console.error("❌ Error fetchData:", err);
-            });
+                .then(res => res.json())
+                .then(res_data => {
+                    if (res_data.success) {
+                        this.donors = res_data.data;
+                        this.totalPages = res_data.pagination.totalPages;
+                        this.totalItems = res_data.pagination.totalItems;
+                        this.itemsPerPage = res_data.pagination.itemsPerPage;
+                    }
+                })
+                .catch(err => {
+                    console.error("❌ Error fetchData:", err);
+                });
         },
         changePage(page) {
             if (page >= 1 && page <= this.totalPages) {
@@ -130,41 +128,41 @@ export default {
         }
     }
 };
-    
-    
-    
-    </script>
+
+
+
+</script>
 
 
 
 <style scoped>
 .quote-item {
-  position: relative;
+    position: relative;
 }
 
 .quote-item span.lable {
-  font-size: 13px;
-  font-weight: 600;
-  display: inline-block;
-  text-transform: capitalize;
-  position: relative;
-  margin-bottom: 5px;
+    font-size: 13px;
+    font-weight: 600;
+    display: inline-block;
+    text-transform: capitalize;
+    position: relative;
+    margin-bottom: 5px;
 }
 
 /* Gaya untuk elemen select */
 .quote-item select {
-  border: 2px solid #F9F9F9;
-  padding: 12px;
-  width: 100%;
-  color: #1A3D7D;
-  font-size: 13px;
-  margin-bottom: 30px;
-  border-radius: 6px;
-  background-color: #F9F9F9;
+    border: 2px solid #F9F9F9;
+    padding: 12px;
+    width: 100%;
+    color: #1A3D7D;
+    font-size: 13px;
+    margin-bottom: 30px;
+    border-radius: 6px;
+    background-color: #F9F9F9;
 }
 
 .quote-item select:focus {
-  border-color: #13ADE5;
-  outline: none;
+    border-color: #13ADE5;
+    outline: none;
 }
 </style>
